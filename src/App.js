1fab45008d83e08b9d3dc5b8a0ca53dfa05e97d4
Maxime Princelle <maxime.princelle@etu.unistra.fr>
2019-04-03 22:58:05 +0200

    Fix design

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Robot from "./model/Robot"
 import Welcome from './view/welcome';
 import Question from './view/question';
 import { Container } from 'react-bootstrap';
+import "./App.css";
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App" class="container">
+			<div className="App" class="App">
 					<Question/>
 			</div>
 		);
