0164a3ac43e996c6509d7f482f2a521431e28d37
Thomas Eyermann <thomas99.eyermann@gmail.com>
2019-04-03 17:03:31 +0200

    creation de packages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import {Container} from 'react-bootstrap';
-import Situation from "./SituationComponent"
-import Robot from "./Robot"
-import Welcome from './welcome';
-import Question from './question';
+import Situation from "./view/SituationComponent"
+import Robot from "./model/Robot"
+import Welcome from './view/welcome';
+import Question from './view/question';
 
 class App extends Component {
 
