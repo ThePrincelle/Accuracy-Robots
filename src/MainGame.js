b98b0d41cb046812caa9a08447558783a0d4e081
[Jean-Thavorak] [PHE] <jtphe@unistra.fr>
2019-04-04 14:18:49 +0200

    Merge

--- src/MainGame.js
+++ src/MainGame.js
@@ -3,7 +3,7 @@ import JsonData from './data/tableaux.json'
 import Question from './components/QuestionComponent';
 import Answer from './components/AnswerComponent';
 import Law from './components/law';
-import { Card, ProgressBar, Modal, Button } from 'react-bootstrap';
+import { Card, ProgressBar, Modal, Button, Image } from 'react-bootstrap';
 
 export default class Game extends Component {
     state = {
@@ -173,9 +173,9 @@ export default class Game extends Component {
 
                 </Card.Body>
             </Card>
-
             {main}
-
+            
+            
             <Law sidebarOpen='false'></Law>
             </>
         )
