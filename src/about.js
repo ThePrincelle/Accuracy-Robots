3ae3a184ddf5ccf4c674c2ce24af35770c574687
Quentin Schaeffer <quentin.Schaeffer2@etu.unistra.fr>
2019-04-04 18:50:12 +0200

    fin de la page des crédits

--- src/about.js
+++ src/about.js
@@ -8,24 +8,25 @@ export default class About extends Component {
             <Row>
                 <Col>
                     <Card style={{width: 200}}>
+                    <Card.Img variant="top" src="/images/jean_phe.jpg" />
+                    <Card.Body>
+                        <Card.Title>Jean Phé</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                    </Card>;
+                </Col>
+
+                <Col>
+                    <Card style={{width: 200}}>
                     <Card.Img variant="top" src="/images/eyermann.png" />
                     <Card.Body>
                         <Card.Title>Thomas Eyermann</Card.Title>
                         <Card.Text>
                             Ne rien faire, mais le faire bien
                         </Card.Text>
-                    </Card.Body>
-                    </Card>;
-                </Col>
-                <Col>
-                    <Card style={{width: 200}}>
-                    <Card.Img variant="top" src="/images/maxime.jpg" />
-                    <Card.Body>
-                        <Card.Title>Maxime Princelle</Card.Title>
-                        <Card.Text>
-                        Les meilleures choses qui arrivent dans le monde de l’entreprise ne sont pas le résultat du travail d’un seul homme. C’est le travail de toute une équipe.
-                        </Card.Text>
-                        <Button variant="primary" href="https://princelle.org/">En Savoir plus</Button>
                     </Card.Body>
                     </Card>;
                 </Col>
@@ -43,17 +44,18 @@ export default class About extends Component {
                 </Col>
                 <Col>
                     <Card style={{width: 200}}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Img variant="top" src="/images/maxime.jpg" />
                     <Card.Body>
-                        <Card.Title>Jean Phé</Card.Title>
+                        <Card.Title>Maxime Princelle</Card.Title>
                         <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
+                        Les meilleures choses qui arrivent dans le monde de l’entreprise ne sont pas le résultat du travail d’un seul homme. C’est le travail de toute une équipe.
                         </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        <Button variant="primary" href="https://princelle.org/">En Savoir plus</Button>
                     </Card.Body>
                     </Card>;
                 </Col>
+                
+                
             </Row>
             </div>
         )
