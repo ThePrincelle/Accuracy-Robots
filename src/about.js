0e085803f191a25d103049a2a1729c9b30ef1947
Quentin Schaeffer <quentin.Schaeffer2@etu.unistra.fr>
2019-04-04 18:17:37 +0200

    ajout d'images

--- src/about.js
+++ src/about.js
@@ -7,44 +7,42 @@ export default class About extends Component {
             <div style={{padding:200}}>
             <Row>
                 <Col>
-                    <Card style={{width: 200, height: 400}}>
+                    <Card style={{width: 200, height: 450}}>
                     <Card.Img variant="top" src="holder.js/100px180" />
                     <Card.Body>
                         <Card.Title>Thomas Eyermann</Card.Title>
                         <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
+                            Ne rien faire, mais le faire bien
                         </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
                     </Card.Body>
                     </Card>;
                 </Col>
                 <Col>
-                    <Card style={{width: 200, height: 400}}>
+                    <Card style={{width: 200, height: 450}}>
                     <Card.Img variant="top" src="/images/maxime.jpg" />
                     <Card.Body>
                         <Card.Title>Maxime Princelle</Card.Title>
                         <Card.Text>
                         
                         </Card.Text>
-                        <Button variant="primary">En Savoir plus</Button>
+                        <Button variant="primary" href="https://princelle.org/">En Savoir plus</Button>
                     </Card.Body>
                     </Card>;
                 </Col>
                 <Col>
-                    <Card style={{width: 200, height: 400}}>
+                    <Card style={{width: 200, height: 450}}>
                     <Card.Img variant="top" src="/images/quentin.png" style={{height:200, width : 200}}/>
                     <Card.Body>
                         <Card.Title>Quentin Schaeffer</Card.Title>
                         <Card.Text>
                             Il n'y a pas de projets urgents, que des projets en retard
                         </Card.Text>
-                        <Button variant="primary">En Savoir plus</Button>
+                        <Button variant="primary" href="https://quentinschaeffer.fr">En Savoir plus</Button>
                     </Card.Body>
                     </Card>;
                 </Col>
                 <Col>
-                    <Card style={{width: 200, height: 400}}>
+                    <Card style={{width: 200, height: 450}}>
                     <Card.Img variant="top" src="holder.js/100px180" />
                     <Card.Body>
                         <Card.Title>Jean Phé</Card.Title>
