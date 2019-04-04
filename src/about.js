f07e311022e7ef69ecb0bf1f819b73ef9aeffdff
Quentin Schaeffer <quentin.Schaeffer2@etu.unistra.fr>
2019-04-04 14:28:48 +0200

    ajout colonne et lignes pour card

--- src/about.js
+++ src/about.js
@@ -1,20 +1,64 @@
 import React, { Component } from 'react';
+import { Card, Button, Row, Col} from 'react-bootstrap';
 
 export default class About extends Component {
     render () {
         return (
             <>
-            <Card>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-            </Card>;
+            <Row>
+                <Col>
+                    <Card>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                    </Card>;
+                </Col>
+                <Col>
+                    <Card>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                    </Card>;
+                </Col>
+                <Col>
+                    <Card>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                    </Card>;
+                </Col>
+                <Col>
+                    <Card>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                    </Card>;
+                </Col>
+            </Row>
             </>
         )
     }
