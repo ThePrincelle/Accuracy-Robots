271e13eecf164fc630772bc96881761e14e3b970
Quentin Schaeffer <quentin.Schaeffer2@etu.unistra.fr>
2019-04-04 14:28:27 +0200

    ajout route

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./css/App.css";
 import Welcome from './welcome';
 import Game from './MainGame';
+import About from './about';
 
 function Accueil() {
   return (<Welcome/>);
@@ -12,11 +13,15 @@ function Main() {
   return (<Game/>);
 }
 
+function about(){
+  return (<About/>)
+}
 function AppRouter() {
   return (
     <Router>
         <Route path="/" exact component={Accueil} />
         <Route path="/game/" component={Main} />
+        <Route path="/about/" component={about}/>
     </Router>
   );
 }
