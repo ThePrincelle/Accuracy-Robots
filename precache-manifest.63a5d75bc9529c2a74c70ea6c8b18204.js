32fcab01ea841807dbdb4d5ead321dec6348f8e1
ThePrincelle <ThePrincelle@users.noreply.github.com>
2022-08-08 08:38:41 +0000

    deploy: 55f9a9b97b0d361db129f34d6cfcbaedd9b9ac9e

---
+++ precache-manifest.63a5d75bc9529c2a74c70ea6c8b18204.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "64c622d941e82143b9cd",
+    "url": "/static/css/main.fa2e2815.chunk.css"
+  },
+  {
+    "revision": "64c622d941e82143b9cd",
+    "url": "/static/js/main.81814f50.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a90847abb905333e48ef",
+    "url": "/static/css/2.e8e3cedc.chunk.css"
+  },
+  {
+    "revision": "a90847abb905333e48ef",
+    "url": "/static/js/2.e8ea5b10.chunk.js"
+  },
+  {
+    "revision": "7dc0c6bdfc4b1a46fa6979239403b1bb",
+    "url": "/static/media/robot.7dc0c6bd.png"
+  },
+  {
+    "revision": "838b2a40d7478fec5663283a7690c170",
+    "url": "/index.html"
+  }
+];
